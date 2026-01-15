87b5f15c9622faca73749c81b9d0695a10af1454
samiralam <samiralam7005@gmail.com>
2026-01-15 20:36:43 +0530

    edit projects

--- src/components/Projects/Slider.jsx
+++ src/components/Projects/Slider.jsx
@@ -17,38 +17,26 @@ const data = [
     href: "https://github.com/samiralam04/ai-resume-chatbot",
   },
 
-  {
-    img: "https://mvd.kerala.gov.in/sites/default/files/citizen_corner/dashboards.jpg",
-    disc: "A visually engaging dashboard developed with HTML, CSS, and JavaScript, utilizing the Spotify Student API to display trending song details. Users can explore up-to-date information on popular tracks, enhancing their music discovery experience.",
-    href: "https://samiralam04.github.io/Spotify_Dashboard/",
-  },
+
   {
     img: "https://mms.businesswire.com/media/20201216005187/en/846780/23/Storyboard_Official_Logo_%281%29.jpg",
     disc: "A versatile platform allowing users to write, edit, and manage their stories. Features include uploading thumbnail images, reading and deleting stories, and downloading them as PDFs.",
     href: "https://samiralam04.github.io/Story-Board/",
   },
-  {
-    img: "https://png.pngtree.com/png-clipart/20200710/original/pngtree-fitness-vector-logo-design-template-design-for-gym-and-fitness-vector-png-image_4246869.jpg",
-    disc: "The Gym Landing Page, crafted with HTML/CSS, presents a fictional gym with intuitive navigation and vibrant visuals, providing easy access to membership details and class schedules. Demonstrating front-end proficiency.",
-    href: " https://samiralam04.github.io/The-Fitness-Club/",
-  },
+
   {
     img: "https://mir-s3-cdn-cf.behance.net/projects/404/8eec89176281219.Y3JvcCw4MDgsNjMyLDAsMA.png",
     disc: " Tiny URL Shortener, A Java servlet-based application that shortens long URLs. It includes user registration, login, and session management, with all data stored securely in a PostgreSQL database.",
     href: "https://github.com/samiralam04/TinyUrl",
   },
 
-  {
-    img: "https://img.freepik.com/premium-vector/lettering-modern-hand-written-text-sticker-planner-bright-text-planning-concept-vector-illustration_565728-492.jpg",
-    disc: "A minimalist To-Do List built with HTML, CSS, and JavaScript, facilitating efficient task management with intuitive design and dynamic functionality.",
-    href: " https://samiralam04.github.io/To-Do-List/",
-  },
+
 
   {
     img: "https://www.logoai.com/uploads/output/2022/10/17/9135d68826af2d0802c03554d508446e.jpg",
     disc: " A React.js and Firebase-based chat app with real-time messaging, user photo uploads, and blocking features. Designed for desktop use, it enhances security with .env-managed API keys and offers a streamlined user experience. ",
     href: "https://samiralam04.github.io/Real-Time-chatApp/",
-  },  
+  },
   {
     img: "https://imgs.search.brave.com/rCD8OBatef1_iNUlkBv97AKcniF5x06ZAA2oIaKG9ds/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/YmlvaW50ZXJhY3Rp/dmUub3JnL3NpdGVz/L2RlZmF1bHQvZmls/ZXMvc3R5bGVzL3Nx/dWFyZV90aHVtYm5h/aWwvcHVibGljL21l/ZGlhL2ltYWdlLzIw/MjEtMDUvRGF0YSUy/MEV4cGxvcmVyJTIw/dGh1bWJuYWlsLmpw/Zz9pdG9rPUlBS2xY/b0Jo",
     disc: "Product Data Explorer is a full-stack app that scrapes and organizes book data from World of Books using Playwright. It features background job processing, a PostgreSQL database, and a modern Next.js interface to explore clean, deduplicated product data. ",
